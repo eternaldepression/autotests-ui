ff8d663c6abda672678bb803f424cec119e331dd
EternalDepression <blackshift99@mail.ru>
2026-02-04 18:33:47 +0300

    Практикуемся в реализации pagecomponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -19,29 +19,30 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 def test_create_course(create_courses_page: CreateCoursePage, courses_list_page: CoursesListPage):
     create_courses_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
 
-    create_courses_page.check_visible_create_course_title()
-    create_courses_page.check_disabled_create_course_button()
+    create_courses_page.create_course_toolbar_view_component.check_visible(is_create_course_disabled=True)
     create_courses_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_courses_page.check_visible_create_course_form(
+    create_courses_page.create_course_form_component.check_visible(
         title="",
         estimated_time="",
         description="",
         max_score="0",
         min_score="0"
     )
-    create_courses_page.check_visible_exercises_title()
-    create_courses_page.check_visible_create_exercise_button()
+    create_courses_page.create_course_exercises_toolbar_view_component.check_visible()
     create_courses_page.check_visible_exercises_empty_view()
     create_courses_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
     create_courses_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_courses_page.fill_create_course_form(
+    create_courses_page.create_course_form_component.fill(
         title="Playwright",
         estimated_time="2 weeks",
         description="Playwright",
         max_score="100",
         min_score="10"
     )
-    create_courses_page.click_create_course_button()
+
+    create_courses_page.create_course_exercises_toolbar_view_component.click_create_exercise_button()
+
+    create_courses_page.create_course_toolbar_view_component.click_create_course_button()
 
     courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(
