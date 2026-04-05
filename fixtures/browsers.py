43bab16c5e5fb39fe043dd0e604fad920b285748
EternalDepression <blackshift99@mail.ru>
2026-04-05 22:22:54 +0300

    Changes

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -8,9 +8,13 @@ from tools.playwright.pages import initialize_playwright_page
 from config import settings
 
 
-@pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+@pytest.fixture(params=settings.browsers)
+def page(request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param
+    )
 
 
 @pytest.fixture(scope="session")
@@ -32,10 +36,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture(scope="function")
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+@pytest.fixture(scope="function", params=settings.browsers)
+def page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
+        browser_type=request.param,
         storage_state=settings.browser_state_file
     )
