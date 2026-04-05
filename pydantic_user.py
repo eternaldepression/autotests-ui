465ee75d14c55fa0a3a0f06a29243aad2e7e6608
EternalDepression <blackshift99@mail.ru>
2026-04-05 14:45:15 +0300

    Изменения

---
+++ pydantic_user.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool = True
+
+
+user_data = {
+    "id": 1,
+    "username": "zara",
+    "email": "zara.bond@gmail.com"
+}
+
+user = User(**user_data)
+print(user)
+print(user.is_active)
+
+invalid_user_data = {
+    "id": "one",
+    "username": "zara",
+    "email": "zara.bond@gmail.com"
+}
+
+invalid_user = User(**invalid_user_data)
