be957ebe6b72303120a2d5bf61c0b4cb23ab9501
EternalDepression <blackshift99@mail.ru>
2026-04-11 23:46:25 +0300

    logs

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,19 +2,32 @@ from playwright.sync_api import Page, expect
 import allure
 from typing import Pattern
 
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
+
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
     def visit(self, url: str):
-        with allure.step(f'Opening the url "{url}"'):
+        step = f'Opening the url "{url}"'
+
+        with allure.step(step):
             self.page.goto(url, wait_until='networkidle')
+            logger.info(step)
 
     def reload(self):
-        with allure.step(f'Reloading page with url "{self.page.url}"'):
+        step = f'Reloading page with url "{self.page.url}"'
+
+        with allure.step(step):
             self.page.reload(wait_until='networkidle')
+            logger.info(step)
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        with allure.step(step):
             expect(self.page).to_have_url(expected_url)
+            logger.info(step)
