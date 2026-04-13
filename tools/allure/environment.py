c06c6018f1c3d5052ecf1b308f2420d87141fa4d
EternalDepression <blackshift99@mail.ru>
2026-04-13 18:26:49 +0300

    Добавляем информацию о переменных окружения в allure отчет

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,9 +1,15 @@
+import sys
+import platform
+
 from config import settings
 
 
 def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
-    properties = '\n'.join(items)
+    os_info = [f'os_info={platform.system()}, {platform.release()}']
+    python_version = [f'python_version={sys.version}']
+
+    properties = '\n'.join([*items, *os_info, *python_version])
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties)
