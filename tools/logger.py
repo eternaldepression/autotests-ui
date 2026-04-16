7bd9e8836e82a9642370284828e348c50ffe77e6
EternalDepression <blackshift99@mail.ru>
2026-04-16 13:50:47 +0300

    Added run autotests on CI 1

--- tools/logger.py
+++ tools/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 def get_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger()
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     handler = logging.StreamHandler()
